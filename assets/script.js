76d54d32b72c037c438758ebc64346fdb7895b81
DavideTarantino <davidetarantino2005@gmail.com>
2024-01-19 14:43:23 +0100

    Compilazione dell'array

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+const dati = [
+    {
+        id : 'barnett',
+        nome : 'Wayne Barnett',
+        ruolo : 'Founder & CEO',
+        foto : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        id : 'carrol',
+        nome : 'Angela Carrol',
+        ruolo : 'Chief Editor',
+        foto : 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        id : 'gordon',
+        nome : 'Walter Gordon',
+        ruolo : 'Office Manager',
+        foto : 'walter-gordon-office-manager.jpg',
+    },
+    {
+        id : 'lopez',
+        nome : 'Angela Lopez',
+        ruolo : 'Social Media Manager',
+        foto : 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        id : 'estrada',
+        nome : 'Scott Estrada',
+        ruolo : 'Developer',
+        foto : 'scott-estrada-developer.jpg',
+    },
+    {
+        id : 'ramos',
+        nome : 'Barbara Ramos',
+        ruolo : 'Graphic Designer',
+        foto : 'barbara-ramos-graphic-designer.jpg',
+    }
+]
